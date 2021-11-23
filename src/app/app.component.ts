2e934284c64d1383e2b3837233db882ff1936048
Eimantas <eim.kasp@gmail.com>
2021-11-23 19:39:33 +0200

    Frontend dalies uzbaigimas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'quiz';
 
-  public answers : Array<any> = [
+  public answers: Array<any> = [
     "Atsakymas 1",
     "Atsakymas 2",
     "Atsakymas 3",
     "Atsakymas 4",
   ];
 
-  constructor() {
+  // Sukuriamas datos objekas
+  public today: any = new Date();
+  public currentQuestion : number = 0;
 
+  // Kintamasis saugoti kiek % klausimu yra atsakyta
+  public progress : number = 0;
+
+  constructor() {
+    console.log("Siandienos data: ");
+    console.log(this.today);
+  }
+
+
+  nextQuestion() {
+    this.currentQuestion++;
+    this.progress = this.currentQuestion / this.answers.length  * 100;
+    console.log("Progress: " + this.progress);
   }
 }
