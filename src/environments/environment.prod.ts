fb88e584c936ffab1986510208f8903d4676524b
Eimantas <eim.kasp@gmail.com>
2021-11-23 20:13:07 +0200

    Angular Fire Instaliavimas
    Dokumentacija:
    https://github.com/angular/angularfire/blob/master/docs/install-and-setup.md

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'bit-quiz-angular',
+    appId: '1:394815752726:web:fe477a08a2bc01af658337',
+    databaseURL: 'https://bit-quiz-angular-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'bit-quiz-angular.appspot.com',
+    apiKey: 'AIzaSyDnxdM-p6MRAY03skBpLZIdlZaqSpruaAU',
+    authDomain: 'bit-quiz-angular.firebaseapp.com',
+    messagingSenderId: '394815752726',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bit-quiz-angular',
+    appId: '1:394815752726:web:fe477a08a2bc01af658337',
+    databaseURL: 'https://bit-quiz-angular-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'bit-quiz-angular.appspot.com',
+    apiKey: 'AIzaSyDnxdM-p6MRAY03skBpLZIdlZaqSpruaAU',
+    authDomain: 'bit-quiz-angular.firebaseapp.com',
+    messagingSenderId: '394815752726',
+  },
   production: false
 };
 
